1656f618a7a85758bfc049521733debf4d2e0070
Wissam K <slowknight3@gmail.com>
2015-04-27 22:21:12 -0700

    Added functionality for extracting data from CSV file in Converter class. Testing in index page.

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
 
-echo "Hello";
+// Dependencies
+require_once 'services/Converter.php';
+
+$converter = new Converter();
+
+// Extracting data from csv
+
+$filename = "data/mini.csv";
+
+$handle = fopen($filename, "r");
+
+if ( $handle !== FALSE ) {
+	$out_arr = $converter::extractData($handle);
+	echo "<pre>";
+	print_r($out_arr);
+	echo "</pre>";
+}
+
+// Converting data to xml
+
+
---
+++ services/Converter.php
@@ -0,0 +1,19 @@
+<?php
+
+class Converter {
+	
+	// private static $option;
+	
+	public static function extractData ($handle, $option=null)
+	{
+		$out_arr = array();
+		while ( ($data = fgetcsv($handle)) !== FALSE ) {
+			$out_arr[] = $data;
+		}
+		return $out_arr;
+	}
+	
+}
+
+
+
