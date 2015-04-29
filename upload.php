17fdc932ec6b17517a840d73de8655156156cb91
Wissam K <slowknight3@gmail.com>
2015-04-29 08:16:43 -0700

    minor changes to upload.php controller.

--- upload.php
+++ upload.php
@@ -19,6 +19,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 			
 			$handle = fopen($file['tmp_name'], "r");
 			
+			// Converting csv content to array
 			if ( $handle !== FALSE ) {
 				$out_arr = $fileUtil::extractData($handle);
 			}
@@ -26,6 +27,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 			if ( !empty($out_arr) ) {
 				$converter = new Converter("1.0", "UTF-8");
 				
+				// Generating XML
 				$xml_str = $converter->generateXML($out_arr);
 				
 				if ( !empty($xml_str) ) {
@@ -34,10 +36,9 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 					
 					// Render response
 					$file_location = $_SERVER["DOCUMENT_ROOT"] . '/dev/xmlcomposer/' . $xml_file_output;
-					// var_dump($file_location);
 					
+					// Download file
 					if (file_exists($file_location)) {
-						// die('hola');
 			            header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
 			            header("Cache-Control: public"); // needed for i.e.
 			            header("Content-Type: application/xml");
